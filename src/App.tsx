7b394733ca697327cb8321df56b0c316a6051a70
Yohan Joo <joo_yohan@fk.cocone.co.jp>
2022-06-01 17:02:18 +0900

    UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [tempUserId, setTempUserId] = useState(null);
+  const [userId, setUserId] = useState(null);
+
+  // for test, skip id setting
+  // const [userId, setUserId] = useState("shield41791");
+
+  function changeTempUserId(event: any) {
+    console.log(event.target.value)
+    setTempUserId(event.target.value)
+  }
+
+  function startChat() {
+    if (tempUserId) {
+      setUserId(tempUserId)
+    }
+    alert(`Hello, ${tempUserId}`)
+  }
+
+  function sendMessage(event: any) {
+    alert("hi")
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {!userId ? (
+        <div>
+          <input 
+            type="text"
+            onChange={(event) => changeTempUserId(event)}
+            ></input>
+          <button onClick={startChat}>start</button>
+        </div>
+      ) : (
+        <div className={`chatArea`}>
+          <div>Your ID : {userId}</div>
+          <div>
+            <textarea 
+              className={`outputTextArea`}
+              readOnly
+            ></textarea>
+          </div>
+          <div className={`inputArea`}>
+            <textarea
+              className={`inputTextArea`}
+            ></textarea>
+            <button 
+              className={`sendButton`}
+              onClick={sendMessage}  
+            >send</button>
+          </div>
+        </div>
+      )}
     </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.tsx</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
